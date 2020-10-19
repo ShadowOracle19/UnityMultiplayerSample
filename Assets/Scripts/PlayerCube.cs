6bb1b86bd2ffb5a7d41094902a6141d2ecb8d414
ShadowOracle19 <lucas.g@hotmail.ca>
2020-10-19 09:34:57 -0400

    stuff

--- Assets/Scripts/PlayerCube.cs
+++ Assets/Scripts/PlayerCube.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Drawing;
 
 public class PlayerCube : MonoBehaviour
 {
     // Start is called before the first frame update
+    public NetworkClient.Player playerRef;
+    public NetworkClient netWorkManRef;
+    public Vector3 Vel;
+    public Camera cubeCamera;
+    public Canvas cubeCanvas;
+
+    public Rigidbody rigidBody;
+    public float speed = 20.0f;
     void Start()
     {
-        
+        if (netWorkManRef.uniqueID.uniqueID == playerRef.id)
+        {
+            cubeCamera.enabled = true;
+            cubeCanvas.enabled = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vel.x = Input.GetAxis("Horizontal");
+        Vel.y = Input.GetAxis("Vertical");
+
+        transform.position += Vel;
+
+        //GetComponent<Renderer>().material.color = new UnityEngine.Color(playerRef.color.R, playerRef.color.G, playerRef.color.B);
+        //GetComponent<Renderer>().material.color = new UnityEngine.Color(playerRef.color.R, playerRef.color.G, playerRef.color.B);
     }
 }
+
